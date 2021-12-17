93d9209843f0b9fee6fccd7829f6f8e9d560507a
dmunofer <diego.fmz@hotmail.com>
2021-12-17 20:30:21 +0100

    comparaproblemas

--- ComparaLosProblemas.py
+++ ComparaLosProblemas.py
@@ -19,6 +19,7 @@ def compareTriplets(a, b):
     lucia_score = 0
     carlos_score = 0
     i=0
+    final=[]
     while i <=2:
         if lucia[i]>carlos[i]:
             lucia_score+=1
@@ -26,7 +27,11 @@ def compareTriplets(a, b):
             carlos_score+=1
         else:
             pass
-    return list(lucia_score, carlos_score)
+        i+=1
+    final.append(lucia_score)
+    final.append(carlos_score)
+    return final
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
     a = list(map(int, input().rstrip().split()))
@@ -35,3 +40,4 @@ if __name__ == '__main__':
     fptr.write(' '.join(map(str, result)))
     fptr.write('\n')
     fptr.close()
+
